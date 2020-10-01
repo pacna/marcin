5a2ad99a3b9e276a2f793bd8f1e266fbc5b0b15b
pacna <31336845+pacna@users.noreply.github.com>
2020-10-01 00:29:47 -0400

    Update ReadMe and implement a is_prime function

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # SDL Ulam Spiral
+
+## Prerequisites
+
+Linux
+```
+apt-get install libsdl2-dev
+```
+
+## How to run
+```
+make
+```
--- main.c
+++ main.c
@@ -1,18 +1,53 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-#define ROW 3
-#define COLUMN 3
+#define ROW 5
+#define COLUMN 5
+
+/** https://en.wikipedia.org/wiki/Primality_test **/
+
+bool is_prime(int num) {
+   if (num <= 3) {
+      return num > 1;
+   } else if (num % 2 == 0 || num % 3 == 0) {
+      return false;
+   }
+
+   int init_num = 5;
+
+   while ((init_num * init_num) <=  num) {
+      if ( num % init_num == 0  || num % (init_num + 2 ) == 0) {
+         return false;
+      }
+
+      init_num+=6;
+   }
+
+   return true;
+}
+
+void print_result(unsigned char input) {
+   if (input != '*') {
+      printf("%d \t", input);
+   } else {
+      printf("%c \t", input);
+   }
+}
 
 int main() {
-   int ulam_spiral[ROW][COLUMN] = {
-      {0, 0, 0},
-      {0, 0, 0},
-      {0, 0, 0}
-   };
+   unsigned char ulam_spiral[ROW][COLUMN] = {'\0'};
+   unsigned char init_num = 1;
 
    for (int i = 0; i < ROW; i++) {
       for (int j = 0; j < COLUMN; j++) {
-         printf("%d", ulam_spiral[i][j]);
+         if (is_prime(init_num)) {
+            ulam_spiral[i][j] = init_num;
+         } else {
+            ulam_spiral[i][j] = '*';
+         }
+         init_num++;
+         print_result(ulam_spiral[i][j]);
       }
       printf("\n");
    }
